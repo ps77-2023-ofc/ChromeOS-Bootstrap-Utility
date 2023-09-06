1908dc93136fd32b6c520540737fa68312fcad6a
Juan David Bogallo Quiroz <juanbogallo1113826@correo.itm.edu.co>
2023-09-06 18:04:39 -0500

    Add builder module Intel Atom Haswell 4Gen
    Add builder module for 4th Gen Intel Atom processors (with codename Haswell)

---
+++ modules/intel/intel_atom_checks/intel_atom_checks_haswell_4Gen.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+
+echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    cat << "Q131-1"
+    Pregunta 1.3.1 Estas son todas las versiones de Chrome OS compatibles con Brunch Framework:
+    1. Chrome OS v86 (codename rammus)
+    2. Chrome OS v87 (codename rammus)
+    3. Chrome OS v88 (codename rammus)
+    4. Chrome OS v89 (codename rammus)
+    5. Chrome OS v90 (codename rammus)
+    6. Chrome OS v91 (codename rammus)
+    7. Chrome OS v92/v93 (codename rammus)
+    8. Chrome OS v94 (codename rammus)
+    9. Chrome OS v96/v97 (codename rammus)
+    10. Chrome OS v98 (codename rammus)
+    Ingresa el número de la versión de Chrome OS que vas a utilizar:
+Q131-1
+# Leer la respuesta del usuario
+read -n 2 haswell_cros_version
+# Limpiar la terminal
+clear
+# Si la versión escogida es 1 (Chrome OS v86), comenzar la descarga de los archivos necesarios
+if [ "$haswell_cros_version" == "1" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r86"
+    echo "VERSIÓN DE CHROME OS: v86"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r86-k5.4-stable-20201201/brunch_r86_k5.4_stable_20201201.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13421.99.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_13421.99.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_cros_version" == "2" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r87"
+    echo "VERSIÓN DE CHROME OS: v87"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r87-stable-20201227/brunch_r87_stable_20201227.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13505.111.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_13505.111.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_cros_version" == "3" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r88"
+    echo "VERSIÓN DE CHROME OS: v88"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r88-stable-20210131/brunch_r88_stable_20210131.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13729.56.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_13729.56.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_cros_version" == "4" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r89"
+    echo "VERSIÓN DE CHROME OS: v89"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r89-stable-20210403/brunch_r89_stable_20210403.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13729.56.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_13729.56.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_cros_version" == "5" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r90"
+    echo "VERSIÓN DE CHROME OS: v90"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r90-stable-20210523/brunch_r90_stable_20210523.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13816.82.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_13816.82.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado." 
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_cros_version" == "6" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r91"
+    echo "VERSIÓN DE CHROME OS: v91"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r91-stable-20210620/brunch_r91_stable_20210620.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13904.55.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_13904.55.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_cros_version" == "7" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r93"
+    cat << "Q14-1"
+    PREGUNTA 1.4 Esta versión de Brunch Framework soporta 2 versiones de Chrome OS al tiempo:
+    1. Chrome OS v92
+    2. Chrome OS v93
+    ¿Cuál de las 2 versiones de Chrome OS deseas usar para crear la imagen?
+Q14-1
+# Leer la respuesta del usuario
+read -n 1 haswell_brunch_version_93_cros_version
+# Limpiar la terminal
+clear
+# Si la versión escogida es 1 (Chrome OS v92), comenzar la descarga de los archivos necesarios
+if [ "$haswell_brunch_version_93_cros_version" == "1" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r93"
+    echo "VERSIÓN DE CHROME OS: v92"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r93-stable-20211002/brunch_r93_stable_20211002.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13982.88.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_13982.88.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_brunch_version_93_cros_version" == "2" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r93"
+    echo "VERSIÓN DE CHROME OS: v93"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r93-stable-20211002/brunch_r93_stable_20211002.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_14092.77.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_14092.77.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+fi
+elif [ "$haswell_cros_version" == "8" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r94"
+    echo "VERSIÓN DE CHROME OS: v94"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r94-stable-20211121/brunch_r94_stable_20211127.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_14150.74.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_14150.74.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_cros_version" == "9" ]; then 
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r97"
+    cat << "Q14-2"
+    PREGUNTA 1.4 Esta versión de Brunch Framework soporta 2 versiones de Chrome OS al tiempo:
+    1. Chrome OS v96
+    2. Chrome OS v97
+    ¿Cuál de las 2 versiones de Chrome OS deseas usar para crear la imagen?
+Q14-2
+# Leer la respuesta del usuario
+read -n 1 haswell_brunch_version_97_cros_version
+# Limpiar la terminal
+clear
+# Si la versión escogida es 1 (Chrome OS v96), comenzar la descarga de los archivos necesarios
+if [ "$haswell_brunch_version_97_cros_version" == "1" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r97"
+    echo "VERSIÓN DE CHROME OS: v96"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r97-stable-20220121/brunch_r97_stable_20220121.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_14268.67.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_14268.67.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+elif [ "$haswell_brunch_version_97_cros_version" == "2" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r97"
+    echo "VERSIÓN DE CHROME OS: v97"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r97-stable-20220121/brunch_r97_stable_20220121.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_14324.62.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_14324.62.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+fi
+elif [ "$haswell_cros_version" == "10" ]; then
+    echo "PROCESADOR OBJETIVO: Intel Atom, plataforma Haswell (4ª Gen)"
+    echo "VERSIÓN DE BRUNCH FRAMEWORK: r100"
+    echo "VERSIÓN DE CHROME OS: v98 (latest)"
+    echo "CODENAME DEL RECOVERY DESTINO: rammus"
+    echo "Descargando archivos necesarios..."
+    wget -q --show-progress "https://github.com/sebanc/brunch/releases/download/r100-stable-20220402/brunch_r100_stable_20220402.tar.gz" -O brunch.tar.gz
+    wget -q --show-progress "https://web.archive.org/web/20220302163230mp_/https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_14388.61.0_rammus_recovery_stable-channel_mp-v2.bin.zip" -O recovery.zip
+    echo "Archivos descargados."
+    echo "Extrayendo archivos..."
+    tar -xzf brunch.tar.gz
+    unzip -qq recovery.zip
+    echo "Archivos extraídos."
+    echo "Renombrando archivo del recovery a recovery.bin..."
+    mv chromeos_14388.61.0_rammus_recovery_stable-channel_mp-v2.bin recovery.bin
+    echo "Archivo del recovery renombrado."
+    echo "Comenzando la construcción de la imagen..."
+    bash "./chromeos-install.sh -src recovery.bin -dst chromeos.img"
+fi
